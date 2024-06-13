0fc702cf62ca8e9c411084b6b87853a70855618a
Annaeldho01 <annaeldho001@gmail.com>
2024-06-14 00:33:50 +0530

    project completed

--- app.js
+++ app.js
@@ -104,7 +104,18 @@ app.post("/login",(req,res)=>{
         
     )
     
-    
+    app.post("/delete",(req,res)=>{
+        let input=req.body
+        busmodel.findByIdAndDelete(input._id).then(
+            (response)=>{
+                res.json({"status":"success"})
+            }
+        ).catch(
+            (error)=>{
+                res.json({"status":"error"})
+            }
+        ).finally()
+    })
 
 app.listen(8080,()=>{
     console.log("server started")
