1165487bc37843c5947efde024b8d271abe4a043
Annaeldho01 <annaeldho001@gmail.com>
2024-06-13 23:28:18 +0530

    project completed

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose=require("mongoose")
 const cors=require("cors")
 const bcrypt=require("bcryptjs")
 const {usermodel}=require("./models/register")
+const jwt=require("jsonwebtoken")
 
 const app=express()
 app.use(cors())
@@ -25,6 +26,39 @@ app.post("/reg",async(req,res)=>{
     register.save()
     res.json({"status":"success"})
 })
+
+app.post("/login",(req,res)=>{
+    let input =req.body
+    //we are checking with mail id
+    usermodel.find({"email":req.body.email}).then(
+        (response)=>{
+            if(response.length>0)
+                {
+                    let dbpass =response[0].password
+                    console.log(dbpass)
+                    bcrypt.compare(input.password,dbpass,(error,isMatch)=>{
+                        if (isMatch) {
+                            //if login success generate token
+                            jwt.sign({email:input.email},"user-app",{expiresIn:"1d"},
+                                (error,token)=>{
+                                if (error) {
+                                    res.json({"status":"unable to create token"})
+                                } else {
+                                    res.json({"status":"success","userid":response[0]._id,"token":token})
+                                }
+                            })
+                        } else {
+                            res.json({"status":"incorrect password"})
+                        }
+                    })
+                }
+            else{
+                res.json({"status":"user not found"})
+            }
+        }
+    )
+    })
+    
 
 app.listen(8080,()=>{
     console.log("server started")
